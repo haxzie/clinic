1ad8ba66843583a4c7d4f94b78422e174b77f1f4
haxzie <musthu.gm@gmail.com>
2025-05-03 23:23:45 +0530

    Fix wrangler build

---
+++ apps/server/src/app.ts
@@ -0,0 +1,17 @@
+import { Hono } from "hono";
+// routes
+import healthRoute from "@/routes/health.route";
+import relayRoute from "@/routes/relay.route";
+import { cors } from "hono/cors";
+
+const app = new Hono();
+app.use(
+  cors({
+    origin: "*", // allow all origins
+  })
+);
+
+app.route("/health", healthRoute);
+app.route("/relay", relayRoute);
+
+export default app;
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,20 +1,6 @@
-import { Hono } from "hono";
-import run from "./server";
+import app from "./app";
+import run from "./utils/server";
 import { env } from "./utils/secrets";
 
-// routes
-import healthRoute from "@/routes/health.route";
-import relayRoute from "@/routes/relay.route";
-import { cors } from "hono/cors";
-
-const app = new Hono();
-app.use(
-  cors({
-    origin: "*", // allow all origins
-  })
-);
-
-app.route("/health", healthRoute);
-app.route("/relay", relayRoute);
-
+// Start the server
 run(app, env.PORT);
