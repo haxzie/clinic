c9247a017af5e46c6876af933ac6eac307ee3bb1
haxzie <musthu.gm@gmail.com>
2025-07-28 19:48:06 +0530

    Fix api list view

--- apps/web-app/src/components/modules/studio/Studio.tsx
+++ apps/web-app/src/components/modules/studio/Studio.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import StudioLayout from "./studio-layout/StudioLayout";
 import ExplorerPanel from "./explorer-panel/ExplorerPanel";
 import TabView from "./tab-view/TabView";
+import { setupShortcuts } from "@/utils/shortCuts";
 
 export default function Studio() {
+  useEffect(() => {
+    document.addEventListener("keydown", setupShortcuts);
+    return () => {
+      document.removeEventListener("keydown", setupShortcuts);
+    };
+  }, []);
   return (
     <StudioLayout>
       <ExplorerPanel />
--- apps/web-app/src/components/modules/studio/api-editor/APIEditor.tsx
+++ apps/web-app/src/components/modules/studio/api-editor/APIEditor.tsx
@@ -1,18 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "./APIEditor.module.scss";
 import RequestBuilder from "./request-builder/RequestBuilder";
 import ResponseViewer from "./response-viewer/ResponseViewer";
 import { PanelGroup } from "react-resizable-panels";
 import { getResizablePanelStorage } from "@/utils/layout";
-import { setupShortcuts } from "@/utils/shortCuts";
 
 export default function APIEditor({ apiId }: { apiId: string }) {
-  useEffect(() => {
-    document.addEventListener("keydown", setupShortcuts);
-    return () => {
-      document.removeEventListener("keydown", setupShortcuts);
-    };
-  }, []);
   return (
     <div className={styles.APIEditor}>
       {/* <APIDetails /> */}
